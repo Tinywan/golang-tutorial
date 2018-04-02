94a31fb7ffbbef7e360ce740072edabf8d6a8972
ShaoBo Wan <overcome.wan@gmail.com>
2018-04-02 22:54:49 +0800

    golang中os/exec包用法

--- main.go
+++ main.go
@@ -2,10 +2,24 @@ package main
 
 import (
     "fmt"
+    "os"
+    "os/exec"
     "github.com/Tinywan/golang-tutorial/stringutil"
 )    
 
 func main() {
     //fmt.Println("Hello golang-tutorial!")
     fmt.Printf(stringutil.Reverse("!oG ,olleH"))
+    cmd := exec.Command("pwd")
+    stdin,err :=cmd.StdinPipe();
+    if err != nil {
+        fmt.Println(err)
+    }
+    _,err = stdin.Write([]byte("tmp.txt"))
+    if err != nil {
+        fmt.Println(err)
+    }
+    stdin.Close()
+    cmd.Stdout = os.Stdout  //终端标准输出tmp.txt
+    cmd.Start()
 }
